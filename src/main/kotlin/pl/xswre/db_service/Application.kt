c990ca56d1c034dfe7cfaea3918636846906762b
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2025-05-27 15:19:50 +0200

    First Kotlin project

---
+++ src/main/kotlin/pl/xswre/db_service/Application.kt
@@ -0,0 +1,11 @@
+package pl.xswre.db_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XsWareDbServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<XsWareDbServiceApplication>(*args)
+}
---
+++ src/test/kotlin/pl/xswre/db_service/XsWareDbServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.xswre.db_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XsWareDbServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
